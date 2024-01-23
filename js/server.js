d0b64118935b86e5d454b1bbc989fdfc1e70c6cc
Aakraos <aakraos@gmail.com>
2024-01-24 00:42:10 +0100

    Changed paths to fetch and modified server.js

--- js/server.js
+++ js/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path'); // Aggiunto questo import
 
 const app = express();
 
 // Configura il middleware express.static per servire file statici dalla directory 'public'
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware per gestire le politiche CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Aggiungi il middleware CORS
 app.use(cors());
@@ -14,7 +22,7 @@ app.get('/', (req, res) => {
   res.send('Benvenuto nel tuo server Express!');
 });
 
-// Avvia il server sulla porta 3000
+// Avvia il server sulla porta 5500
 const port = 5500;
 app.listen(port, () => {
   console.log(`Server Express in ascolto sulla porta ${port}`);
