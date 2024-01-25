c5af133be7b5ecb812df350ac2271bc57b95f871
Aakraos <aakraos@gmail.com>
2024-01-25 02:03:17 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -54,80 +54,79 @@ function myMap() {
   }
 
   // GENERATORE CERTIFICATO
-  console.log("hello");
+console.log("hello");
 
-  const userName = document.getElementById("name");
-  const submitBtn = document.getElementById("submitBtn");
-  
-  submitBtn.addEventListener("click", async () => {
-      const val = userName.value;
-  
-      if (val.trim() !== "" && userName.checkValidity()) {
-          try {
-              const responsePdf = await fetch('https://raw.githubusercontent.com/aakraos/porconesimo/main/js/Certificate.pdf', {
-                  method: 'GET',
-                  mode: 'cors',
-              });
-  
-              if (!responsePdf.ok) {
-                  throw new Error('Errore nella richiesta del PDF: ' + responsePdf.statusText);
-              }
-  
-              const existingPdfBytes = await responsePdf.arrayBuffer();
-  
-              // Utilizza la libreria PDFLib
-              const pdfDoc = await PDFLib.PDFDocument.load(existingPdfBytes);
-  
-              // Correggi il percorso del font
-              const fontBytes = await fetch('https://raw.githubusercontent.com/aakraos/porconesimo/main/js/Sanchez-Regular.ttf', {
-                  method: 'GET',
-                  mode: 'cors',
-              }).then((res) => res.arrayBuffer());
-  
-              const font = await pdfDoc.embedFont(fontBytes);
-  
-              const page = pdfDoc.getPages()[0];
-  
-              const maxWidth = page.getWidth() - 100;
-              const textWidth = font.widthOfTextAtSize(val, 58);
-              const fontSize = textWidth > maxWidth ? (58 * maxWidth) / textWidth : 58;
-  
-              const newTextWidth = font.widthOfTextAtSize(val, fontSize);
-              const newCenterX = (page.getWidth() - newTextWidth) / 2;
-  
-              page.drawText(val, {
-                  x: newCenterX,
-                  y: 520,
-                  size: fontSize,
-                  font: font,
-                  color: PDFLib.rgb(192 / 255, 192 / 255, 192 / 255),
-              });
-  
-              // Aggiungi eventuali altre modifiche al PDF, se necessario
-  
-              const modifiedPdfBytes = await pdfDoc.save();
-              const modifiedPdfBlob = new Blob([modifiedPdfBytes], { type: 'application/pdf' });
-              const modifiedPdfUrl = URL.createObjectURL(modifiedPdfBlob);
-  
-              saveAs(modifiedPdfUrl, "newcertificate.pdf");
-          } catch (error) {
-              console.error(error);
-          }
-      } else {
-          userName.reportValidity();
-      }
-  });
-  
-  function saveAs(uri, filename) {
-      const link = document.createElement("a");
-      link.href = uri;
-      link.download = filename;
-  
-      // Aggiungi il link al documento e simula il clic
-      document.body.appendChild(link);
-      link.click();
-  
-      // Rimuovi il link dal documento
-      document.body.removeChild(link);
-  }
-  
+const userName = document.getElementById("name");
+const submitBtn = document.getElementById("submitBtn");
+
+submitBtn.addEventListener("click", async () => {
+    const val = userName.value;
+
+    if (val.trim() !== "" && userName.checkValidity()) {
+        try {
+            const responsePdf = await fetch('https://raw.githubusercontent.com/aakraos/porconesimo/main/js/Certificate.pdf', {
+                method: 'GET',
+                mode: 'cors',
+            });
+
+            if (!responsePdf.ok) {
+                throw new Error('Errore nella richiesta del PDF: ' + responsePdf.statusText);
+            }
+
+            const existingPdfBytes = await responsePdf.arrayBuffer();
+
+            // Utilizza la libreria PDFLib
+            const pdfDoc = await PDFLib.PDFDocument.load(existingPdfBytes);
+
+            // Correggi il percorso del font
+            const fontBytes = await fetch('https://raw.githubusercontent.com/aakraos/porconesimo/main/js/Sanchez-Regular.ttf', {
+                method: 'GET',
+                mode: 'cors',
+            }).then((res) => res.arrayBuffer());
+
+            const font = await pdfDoc.embedFont(fontBytes);
+
+            const page = pdfDoc.getPages()[0];
+
+            const maxWidth = page.getWidth() - 100;
+            const textWidth = font.widthOfTextAtSize(val, 58);
+            const fontSize = textWidth > maxWidth ? (58 * maxWidth) / textWidth : 58;
+
+            const newTextWidth = font.widthOfTextAtSize(val, fontSize);
+            const newCenterX = (page.getWidth() - newTextWidth) / 2;
+
+            page.drawText(val, {
+                x: newCenterX,
+                y: 520,
+                size: fontSize,
+                font: font,
+                color: PDFLib.rgb(192 / 255, 192 / 255, 192 / 255),
+            });
+
+            // Aggiungi eventuali altre modifiche al PDF, se necessario
+
+            const modifiedPdfBytes = await pdfDoc.save();
+            const modifiedPdfBlob = new Blob([modifiedPdfBytes], { type: 'application/pdf' });
+            const modifiedPdfUrl = URL.createObjectURL(modifiedPdfBlob);
+
+            saveAs(modifiedPdfUrl, "newcertificate.pdf");
+        } catch (error) {
+            console.error(error);
+        }
+    } else {
+        userName.reportValidity();
+    }
+});
+
+function saveAs(uri, filename) {
+    const link = document.createElement("a");
+    link.href = uri;
+    link.download = filename;
+
+    // Aggiungi il link al documento e simula il clic
+    document.body.appendChild(link);
+    link.click();
+
+    // Rimuovi il link dal documento
+    document.body.removeChild(link);
+}
